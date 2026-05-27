31e14600a2dbcf91d7219055eac4c83bcb3516a7
hangngt <hangna204@gmail.com>
2026-05-27 07:28:16 +0700

    add fastapi backend

---
+++ recommend/backend/api/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from ml.hybrid.hybrid_service import HybridService
+
+app = FastAPI()
+
+# Load model khi server start
+hybrid = HybridService()
+
+@app.get("/")
+def home():
+    return {"message": "Travel Recommendation API"}
+
+@app.get("/recommend")
+def recommend(
+    city: str = None,
+    user_id: str = None,
+    lat: float = None,
+    lng: float = None,
+):
+    results = hybrid.recommend(
+        user_id=user_id,
+        city_name=city,
+        user_lat=lat,
+        user_lng=lng,
+        top_k=10
+    )
+
+    return results.to_dict(orient="records")
