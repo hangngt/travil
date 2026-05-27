9c835e35377be2fb616e10c341df90c9a02160ab
hangngt <134864421+hangngt@users.noreply.github.com>
2026-05-27 15:36:43 +0700

    update main

--- recommend/backend/api/main.py
+++ recommend/backend/api/main.py
@@ -1,42 +1,27 @@
-print("🔥 FILE LOADED")
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
-print("1")
 from ml.hybrid.hybrid_service import HybridService
-print("2")
 
 app = FastAPI()
 
 hybrid = None
-
-print("🚀 MAIN.PY IMPORTED")
-
-
-# ======================
-# STARTUP
-# ======================
 @app.on_event("startup")
 def load_model():
     global hybrid
     try:
-        print("🔥 STARTUP START")
         hybrid = HybridService()
-        print("🔥 STARTUP DONE")
     except Exception as e:
         import traceback
-        print("❌ STARTUP ERROR:", str(e))
+        print(" STARTUP ERROR:", str(e))
         print(traceback.format_exc())
         hybrid = None   # KHÔNG raise để tránh kill server
 
 
-# ======================
-# HEALTH CHECK
-# ======================
 @app.get("/")
 def home():
-    print("🏠 HOME CALLED")
+    print("HOME CALLED")
     return {
         "message": "Travel Recommendation API",
         "status": "running",
@@ -52,9 +37,8 @@ def recommend(
     lat: float = None,
     lng: float = None,
 ):
-    print(f"📍 RECOMMEND CALLED | city={city}, user_id={user_id}, lat={lat}, lng={lng}")
+    print(f" RECOMMEND CALLED | city={city}, user_id={user_id}, lat={lat}, lng={lng}")
 
-    # ⚠️ CHECK MODEL BEFORE USE
     if hybrid is None:
         return JSONResponse(
             status_code=503,
@@ -73,12 +57,12 @@ def recommend(
             top_k=10
         )
 
-        print("📊 Recommendation success, rows:", len(results))
+        print("Recommendation success, rows:", len(results))
         return results.to_dict(orient="records")
 
     except Exception as e:
         import traceback
-        print("❌ ERROR IN RECOMMEND:", str(e))
+        print(" ERROR IN RECOMMEND:", str(e))
         print(traceback.format_exc())
 
         return JSONResponse(
